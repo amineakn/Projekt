a6850c6e70ab9748968a844db4973af46200465b
amineakn <amine.akan@student.hnu.de>
2025-01-15 14:49:51 +0100

    Klassen erstellt GUI - Oberfläche erstellt

--- src/Projekt/BurgerLadenForm.java
+++ src/Projekt/BurgerLadenForm.java
@@ -1,4 +1,24 @@
 package Projekt;
 
+import javax.swing.*;
+
 public class BurgerLadenForm {
+    private JPanel frame;
+    private JPanel mainPanel;
+    private JLabel bunLabel;
+    private JLabel toppingsLabel;
+    private JLabel sossenLabel;
+    private JLabel veganLabel;
+    private JComboBox bunComboBox;
+    private JCheckBox tomatenCheckBox;
+    private JCheckBox jalapenoCheckBox;
+    private JCheckBox cheddarCheckBox;
+    private JCheckBox salatCheckBox;
+    private JCheckBox veganCheckBox;
+    private JComboBox sossenComboBox;
+    private JButton calculatePriceButton;
+    private JButton clearSelectionButton;
+    private JLabel preisLabel;
+    private JButton AusgebenButton;
+    private JTextArea textArea1;
 }
